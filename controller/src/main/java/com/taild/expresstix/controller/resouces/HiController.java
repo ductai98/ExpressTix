9310375f637e2454f2aa007b87776a248d56e468
ductai <lductai98@gmail.com>
2025-03-11 13:05:43 +0700

    Config resilience4j ratelimiter and circuitbreaker

--- controller/src/main/java/com/taild/expresstix/controller/resouces/HiController.java
+++ controller/src/main/java/com/taild/expresstix/controller/resouces/HiController.java
@@ -1,20 +1,47 @@
 package com.taild.expresstix.controller.resouces;
 
 import com.taild.expresstix.application.service.event.EventAppService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.security.SecureRandom;
 
 @RestController
-@RequestMapping("/hello")
+@RequestMapping("/api")
 public class HiController {
+
+    private  static final SecureRandom secureRandom = new SecureRandom();
 
     @Autowired
     private EventAppService eventAppService;
 
-    @GetMapping
+    private final RestTemplate restTemplate = new RestTemplate()  ;
+
+    @GetMapping("/hello")
+    @RateLimiter(name = "backendA", fallbackMethod = "fallbackHello")
     public String hello() {
         return eventAppService.sayHi("taild");
+    }
+
+    public String fallbackHello(Throwable throwable) {
+        return "Too many requests";
+    }
+
+    @GetMapping("/circuit/breaker")
+    @CircuitBreaker(name = "checkRandom", fallbackMethod = "fallbackCircuitBreaker")
+    public String circuitBreaker() {
+        int productId = secureRandom.nextInt(20) + 1; // Product ID between 1 and 20
+        String url = "https://fakestoreapi.com/products/" + productId;
+
+        return restTemplate.getForObject(url, String.class);
+    }
+
+    public String fallbackCircuitBreaker(Throwable throwable) {
+        return "Service fakestoreapi is currently unavailable";
     }
 }
